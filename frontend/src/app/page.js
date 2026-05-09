9aaa670d5539a95d6962997fc0e09238730df48f
Aman Jhajharia <141808955+aman-jhajharia@users.noreply.github.com>
2026-05-09 09:28:38 +0530

    Update live course announcement in page.js

--- frontend/src/app/page.js
+++ frontend/src/app/page.js
@@ -24,7 +24,7 @@ export default function Home() {
           animate={{ rotate: -2, scale: 1 }}
           transition={{ delay: 0.3, type: 'spring' }}
         >
-          _Optimization for CS and _Environment Studies Paper are live now!
+          _Operating System, Essentials of Business Management, Digital Electronics, and _Environment Studies Paper are live now!
         </motion.p>
         
         <motion.div 
